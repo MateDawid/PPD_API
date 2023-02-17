eddfdc634c908a20c72a29f9a65cf7403c8d7985
MateDawid <mateusiakdawid@gmail.com>
2023-02-17 21:40:39 +0100

    Transaction model

--- ppd_api/data/models.py
+++ ppd_api/data/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    transaction_id = models.CharField(max_length=40)
+    price = models.IntegerField()
+    transaction_date = models.DateField()
+    postcode = models.CharField(max_length=10)
+    property_type = models.CharField(max_length=1)
+    new_building = models.BooleanField()
+    duration = models.CharField(max_length=1)
+    paon = models.CharField(max_length=256)
+    saon = models.CharField(max_length=256)
+    street = models.CharField(max_length=256)
+    locality = models.CharField(max_length=256)
+    city = models.CharField(max_length=256)
+    district = models.CharField(max_length=256)
+    county = models.CharField(max_length=256)
+    category_type = models.CharField(max_length=128)
+    record_status = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.transaction_id
