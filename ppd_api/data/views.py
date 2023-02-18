cf8daf347d3d985a09e3dfd7a05d10e72175eb2a
Dawid Mateusiak <mateusiakdawid@gmail.com>
2023-02-18 18:09:30 +0100

    Data filtering

--- ppd_api/data/views.py
+++ ppd_api/data/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from data.models import Transaction
@@ -7,3 +8,19 @@ from data.serializers import TransactionSerializer
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    model = serializer_class.Meta.model
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'transaction_id': ['exact'],
+        'price': ['exact', 'gte', 'lte'],
+        'transaction_date': ['exact', 'gte', 'lte'],
+        'postcode': ['exact'],
+        'new_building': ['exact'],
+        'duration': ['exact'],
+        'city': ['exact'],
+        'street': ['exact'],
+        'paon': ['exact'],
+        'saon': ['exact'],
+        'category_type': ['exact'],
+        'record_status': ['exact'],
+    }
--- ppd_api/ppd_api/settings.py
+++ ppd_api/ppd_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'data'
 ]
 
@@ -99,7 +100,10 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
 }
 
 
